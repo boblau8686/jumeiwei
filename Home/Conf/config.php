acadf4561c63be371433b79779457099b0d3219d
Kevin Jiang <kittymiky@gmail.com>
2012-03-25 20:12:21 +0800

    改用ThinkPHP来做

---
+++ Home/Conf/config.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+	'LAYOUT_ON' => 1
+);
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('APP_DEBUG', true);
+
+define('APP_NAME', 'Home');
+
+define('APP_PATH', 'Home/');
+
+require_once './ThinkPHP/ThinkPHP.php';
