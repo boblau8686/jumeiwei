d72c00f2db9d7043622605ae9e0f546faa34d358
Bob <Bob@Bob-PC>
2012-05-07 00:15:41 +0800

    完成我的主页部分的模板制作

---
+++ Lib/Action/Home/CustomerAction.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 客户控制器
+ *
+ * @author Bob
+ */
+class CustomerAction extends Action {
+
+    public function index() {
+        $this->display();
+    }
+
+    public function order() {
+        $this->display();
+    }
+
+    public function collect() {
+        $this->display();
+    }
+
+    public function info() {
+        $this->display();
+    }
+
+}
+
+?>
